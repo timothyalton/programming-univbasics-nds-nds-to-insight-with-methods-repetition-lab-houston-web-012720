6ad12052ea02aafc9d4ec93b84eabcc5eed263fe
Mitchell T Alton <mitchell.alton@live.com>
2020-01-21 23:19:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -44,10 +44,13 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+hash_total = directors_totals(source)
+name = list_of_directors(source)
  total = 0 
  i = 0 
  while i < source.length do 
-   total += directors_totals(source)
+   total += hash_total[name]
   i += 1 
 end
 total
